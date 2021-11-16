a6808066f0cbdd6faadf68e89b21b058f0f669fc
pletain@dgu.ac.kr <pletain@dgu.ac.kr>
2021-11-16 23:26:57 +0900

    servlet

--- servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -16,6 +16,13 @@ public class HelloServlet extends HttpServlet{
         System.out.println("HelloServlet.service");
         System.out.println("request = " + request);
         System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
     }
 
     
