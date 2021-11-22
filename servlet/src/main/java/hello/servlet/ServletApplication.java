99c43233093258929173980890fdc6f7137f1456
pletain@dgu.ac.kr <pletain@dgu.ac.kr>
2021-11-22 20:21:29 +0900

    jsp

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+
 @ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
@@ -11,6 +12,5 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
-
 
 }
