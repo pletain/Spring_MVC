f3fb27b74ced045148a0d3a405b1477e8feaae56
pletain@dgu.ac.kr <pletain@dgu.ac.kr>
2021-11-24 20:24:06 +0900

    war setting

--- servlet/src/main/java/hello/servlet/ServletInitializer.java
+++ servlet/src/main/java/hello/servlet/ServletInitializer.java
@@ -1,13 +1,20 @@
 package hello.servlet;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ServletApplication.class);
+		return application.sources(ServletInitializer.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletInitializer.class, args);
 	}
 
 }
