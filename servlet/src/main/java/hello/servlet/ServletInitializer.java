be546dc00ced7a9bcd3f9bb2b5c989ca103ef130
pletain <pletain@dgu.ac.kr>
2021-11-25 14:52:29 +0000

    jsp

--- servlet/src/main/java/hello/servlet/ServletInitializer.java
+++ servlet/src/main/java/hello/servlet/ServletInitializer.java
@@ -3,8 +3,10 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@ServletComponentScan
 @SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
 
